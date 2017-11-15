044da89e1ddf761ef0a7d3d43965c7d7bab13bf1
hou0101 <32810772+GRM-RM@users.noreply.github.com>
2017-11-15 20:20:50 +0800

    Initial commit

---
+++ dqd-addnews-service/src/main/java/com/dqd/three/App.java
@@ -0,0 +1,13 @@
+package com.dqd.three;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
