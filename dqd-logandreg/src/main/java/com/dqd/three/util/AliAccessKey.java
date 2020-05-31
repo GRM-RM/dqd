2529708f9e5352a322fb51429bccf0b07667edfa
houxudong01 <1046752396@qq.com>
2020-05-31 20:04:05 +0800

    之前码了我的阿里云accesskey，最近一直给我发邮件说我accesskey泄漏，直接删了吧
    之前码了我的阿里云accesskey，最近一直给我发邮件说我accesskey泄漏，直接删了吧，太烦了

--- dqd-logandreg/src/main/java/com/dqd/three/util/AliAccessKey.java
+++
@@ -1,6 +0,0 @@
-package com.dqd.three.util;
-
-public interface AliAccessKey {
-    final String ACCESS_KEY_ID="LTAIiK3QPV2JyULg";
-    final String ACCESS_KEY_SECRET="PRtL3adjlS5UrYr52hYcM45aMb03zn";
-}
